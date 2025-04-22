aad0ada713d883a0b0a84f2c08fde76d81db30e8
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-04-22 10:25:01 +0530

    Time: 10 ms (90.96%) | Memory: 46.2 MB (38.51%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.java
+++ 382-linked-list-random-node/linked-list-random-node.java
@@ -1,18 +1,14 @@
 class Solution {
-    int N = 0;
-    ListNode head = null;
+    ArrayList<Integer> list = new ArrayList<>();
     public Solution(ListNode head) {
-        this.head = head;
+        while(head!=null){
+            list.add(head.val);
+            head = head.next;
+        }
     }
     
     public int getRandom() {
-        ListNode p = this.head;
-        int i = 1, ans = 0;
-        while (p != null) {
-            if (Math.random() * i < 1) ans = p.val; // replace ans with i-th node.val with probability 1/i
-            p = p.next;
-            i ++;
-        }
-        return ans;
+        int ans = (int)(Math.random()*list.size());
+        return list.get(ans);  
     }
 }
