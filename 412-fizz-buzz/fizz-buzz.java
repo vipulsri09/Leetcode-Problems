fb7bbb0c5b4451fe6e1d3aa3f380102d91453770
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-04-25 09:15:59 +0530

    Time: 1 ms (99.83%) | Memory: 44.9 MB (90.10%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List fizzBuzz(int n) {
+        List ans = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            ans.add(
+                i % 15 == 0 ? "FizzBuzz" :
+                i % 5 == 0  ? "Buzz" :
+                i % 3 == 0  ? "Fizz" :
+                String.valueOf(i)
+            );
+        }
+
+        return ans;
+    }
+}
