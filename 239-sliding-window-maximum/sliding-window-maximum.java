8d0762f4797609ffba503b74a6dc973eeb0d6938
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-04-27 09:15:31 +0530

    Time: 38 ms (28.46%) | Memory: 62.3 MB (21.34%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        List<Integer> res = new ArrayList<>();
-        Deque<Integer> deque = new LinkedList<>();
+       List<Integer> res=new ArrayList<>();
+       Deque<Integer> deque = new LinkedList<>();
 
-        for (int idx = 0; idx < nums.length; idx++) {
-            if (!deque.isEmpty() && deque.getFirst() <= idx - k) {
-                deque.pollFirst();
-            }
+       for(int idx=0; idx<nums.length; idx++){
+        int num =nums[idx];
 
-            while (!deque.isEmpty() && nums[deque.getLast()] < nums[idx]) {
-                deque.pollLast();
-            }
-
-            deque.addLast(idx);
-
-            if (idx >= k - 1) {
-                res.add(nums[deque.getFirst()]);
-            }
-        }
-
-        return res.stream().mapToInt(i -> i).toArray();
+        while(!deque.isEmpty() && deque.getLast() <num){
+            deque.pollLast();
+          }
+          deque.addLast(num);
+          if(idx>=k && nums[idx-k] == deque.getFirst()){
+            deque.pollFirst();
+          }
+          if(idx>= k-1){
+            res.add(deque.getFirst());
+          }
+       } 
+       return res.stream(). mapToInt(i -> i).toArray();
     }
 }
