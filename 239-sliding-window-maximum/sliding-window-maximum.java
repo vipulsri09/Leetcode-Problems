6c360234b46ed1cc0ac6b5b1a23cc3b3b6a79d77
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-04-27 09:17:21 +0530

    Time: 8 ms (99.14%) | Memory: 61.3 MB (31.83%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,25 +1,27 @@
-public class Solution {
-    public int[] maxSlidingWindow(int[] a, int k) {
-        if (a == null || k <= 0) {
-            return new int[0];
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length ; 
+        int[] rs = new int[n-k+1] ; 
+        int[] deq = new int[n+1]; 
+        int start = -1 ; 
+        int end = -1 ;  
+        for( int i=0 ; i<k ; ++i ){
+            while( start != end  && nums[ deq[end] ] < nums[i] ){; 
+                end-- ; 
+            }
+            deq[++end] = i ; 
+        } 
+        rs[0] = nums[ deq[ start+1 ] ] ; 
+        for( int i=k ; i<n ; ++i ){
+            while( start != end && deq[start+1] <= i-k  ){
+                start++ ; 
+            }
+            while( start != end && nums[ deq[end] ] < nums[i] ){
+                end-- ; 
+            }
+            deq[++end] = i ;
+            rs[i-k+1] = nums [deq[ start+1 ] ] ; 
         }
-        int n = a.length;
-        int[] r = new int[n - k + 1];
-        int ri = 0;
-        Deque<Integer> q = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            if (!q.isEmpty() && q.peek() < i - k + 1) {
-                q.poll();
-            }
-            while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
-                q.pollLast();
-            }
-            q.offer(i);
-            if (i >= k - 1) {
-                r[ri++] = a[q.peek()];
-            }
-        }
-        return r;
+        return rs ;
     }
 }
