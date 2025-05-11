339e5321b993cb299799611c9138bb9456545464
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-05-11 23:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (24.20%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,44 +1,59 @@
 class Solution {
-    public static int countLive(int[][] board, int row, int col) {
-        int count = 0;
-        int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
-        int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-        for (int i = 0; i < 8; i++) {
-            int newRow = row + dx[i];
-            int newCol = col + dy[i];
-            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
-                if (board[newRow][newCol] == 1 || board[newRow][newCol] == -1) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
     public void gameOfLife(int[][] board) {
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                int liveCnt = countLive(board, i, j);
-                if (board[i][j] == 1 || board[i][j] == -1) {
-                    if (liveCnt < 2 || liveCnt > 3) {
-                        board[i][j] = -1;
-                    }
-                } else {
-                    if (liveCnt == 3) {
-                        board[i][j] = -2;
-                    }
-                }
+        int[][] temp=new int[board.length][board[0].length];
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                temp[i][j]=board[i][j];
             }
         }
+        int m=temp.length;
+        int n=temp[0].length;
 
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                if (board[i][j] == -1) {
-                    board[i][j] = 0;
-                } else if (board[i][j] == -2) {
-                    board[i][j] = 1;
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+            int cnt0=0;
+             int cnt1=0;
+             int hr1=-1,hr2=-1,hr3=-1,hr4=-1,hr5=-1,hr6=-1,hr7=-1,hr8=-1,hr9=-1;
+             if(i-1>=0&&j-1>=0) hr1=temp[i-1][j-1];
+             if(hr1==0) cnt0++;
+             else if(hr1==1) cnt1++;
+
+             if(i-1>=0)  hr2=temp[i-1][j];
+             if(hr2==0) cnt0++;
+             else if(hr2==1)cnt1++;
+
+             if(i-1>=0&&j+1<n) hr3=temp[i-1][j+1];
+             if(hr3==0) cnt0++;
+             else if(hr3==1)cnt1++;
+
+             if(j-1>=0) hr4=temp[i][j-1];
+             if(hr4==0) cnt0++;
+             else if(hr4==1)cnt1++;
+
+             if(j+1<n)  hr6=temp[i][j+1];
+             if(hr6==0) cnt0++;
+             else if(hr6==1)cnt1++;
+
+             if(i+1<m&&j-1>=0)  hr7=temp[i+1][j-1];
+             if(hr7==0) cnt0++;
+             else if(hr7==1)cnt1++;
+
+             if(i+1<m)  hr8=temp[i+1][j];
+             if(hr8==0) cnt0++;
+             else if(hr8==1)cnt1++;
+
+             if(i+1<m&&j+1<n)  hr9=temp[i+1][j+1];
+             if(hr9==0) cnt0++;
+             else if(hr9==1)cnt1++;
+
+         if(temp[i][j]==0){ if (cnt1==3) board[i][j]=1;}
+         else if(temp[i][j]==1){
+
+        if(cnt1<2||cnt1>3){
+            board[i][j]=0;
+        }}
+             
+              
             }
         }
     }
