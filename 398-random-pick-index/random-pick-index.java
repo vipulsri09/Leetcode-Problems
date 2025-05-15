f2744b54ae1c460269e2afd8d4b41bc0fa176980
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-05-15 14:49:25 +0530

    Time: 1024 ms (33.13%) | Memory: 55.4 MB (84.57%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -1,21 +1,17 @@
 class Solution {
-    Map<Integer,List<Integer>>map;
+    int[] nums;
+
     public Solution(int[] nums) {
-        this.map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.putIfAbsent(nums[i],new ArrayList<>());
-            map.get(nums[i]).add(i);
-        }
+        this.nums=nums;
     }
     
     public int pick(int target) {
-        int n=map.get(target).size();
-        return map.get(target).get((int)(Math.random()*n));
+        List<Integer> l=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                l.add(i);
+            }
+        }
+        return l.get((int)(Math.random()*(l.size())));
     }
 }
-
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(nums);
- * int param_1 = obj.pick(target);
- */
