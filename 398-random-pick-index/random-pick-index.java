51e3fc7aceae1ae6df3224628c741d90bd8618a1
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-05-15 14:53:44 +0530

    Time: 81 ms (58.85%) | Memory: 58.9 MB (7.33%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -1,18 +1,32 @@
+import java.util.*;
+
 class Solution {
-    Map<Integer, List<Integer>> indexMap;
-    Random random;
+    private final Map<Integer, int[]> map = new HashMap<>();
+    private final Random random = new Random();
 
     public Solution(int[] nums) {
-        indexMap = new HashMap<>();
-        random = new Random();
-
+        Map<Integer, List<Integer>> tempMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            indexMap.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+            int val = nums[i];
+            List<Integer> list = tempMap.get(val);
+            if (list == null) {
+                list = new ArrayList<>();
+                tempMap.put(val, list);
+            }
+            list.add(i);
+        }
+        for (Map.Entry<Integer, List<Integer>> entry : tempMap.entrySet()) {
+            List<Integer> list = entry.getValue();
+            int[] arr = new int[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                arr[i] = list.get(i);
+            }
+            map.put(entry.getKey(), arr);
         }
     }
 
     public int pick(int target) {
-        List<Integer> indices = indexMap.get(target);
-        return indices.get(random.nextInt(indices.size()));
+        int[] indices = map.get(target);
+        return indices[random.nextInt(indices.length)];
     }
 }
