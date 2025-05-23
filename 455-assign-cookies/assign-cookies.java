82b6be0f4814b631a8d199276bbcd003f23cff96
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-05-23 14:02:57 +0530

    Time: 11 ms (39.57%) | Memory: 48.8 MB (24.83%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,19 +1,11 @@
 class Solution {
-    public int findContentChildren(final int[] g, final int[] s) {
-        int i = g.length - 1, j = s.length - 1, count = 0;
-
+    public int findContentChildren(int[] g, int[] s) {
+        int i = 0;
         Arrays.sort(g);
         Arrays.sort(s);
-
-        while(i >= 0 && j >= 0) {
-            final int greed = g[i], size = s[j];
-
-            if(size >= greed)
-                j--;
-
-            i--;
+        for(int j=0; i<g.length && j<s.length; j++){
+            if(g[i]<=s[j])  i++;
         }
-
-        return s.length - j - 1;
+        return i;
     }
 }
