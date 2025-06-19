8b35e3f3437340152b945b78cf013a1096579c7b
Vipul Srivastava <vipulsrivastava901@gmail.com>
2025-06-19 11:43:08 +0530

    Time: 15 ms (99.01%) | Memory: 45.3 MB (95.79%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.java
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.java
@@ -1,20 +1,24 @@
 class Solution {
     public long numberOfWays(String s) {
-        long count0 = 0, count1 = 0;
-        long count01 = 0, count10 = 0;
-        long count010 = 0, count101 = 0;
-        
-        for (char ch : s.toCharArray()) {
-            if (ch == '0') {
-                count0++;
-                count10 += count1;
-                count010 += count01;
-            } else {
-                count1++;
-                count01 += count0;
-                count101 += count10;
+        long zeroes=0;
+        long ones=0;
+        long zeroOne=0;
+        long oneZero=0;
+        char[] arr=s.toCharArray();
+        long ans=0;
+        for(char c:arr){
+            if(c=='0'){
+                ans=ans+zeroOne;
+                oneZero=oneZero+ones;
+                zeroes++;
+
+            }
+            else{
+                ans=ans+oneZero;
+                zeroOne=zeroOne+zeroes;
+                ones++;
             }
         }
-        return count010 + count101;
+        return ans;
     }
 }
